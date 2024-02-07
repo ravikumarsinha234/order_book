be22f2bcaf01436570945078866cbe5c4accc646
Ravi <ravikumarsinha234@gmail.com>
2024-02-07 22:38:08 +0530

    order book

--- order_class.py
+++ order_class.py
@@ -1,6 +1,6 @@
 class EachOrder:
     def __init__(self,order_id,side,price,quantity):
-        self.order_id = order_id #assuming order id to be unique
+        self.order_id = order_id #assuming order_id to be unique
         self.side = side # It will be 'B' for buy and 'S' for sell
         self.price = price
         self.quantity = quantity
@@ -9,7 +9,7 @@ class OrderBook:
     def __init__(self):
         self.orders={}
 
-    def OnNewOrder(self,order):
+    def OnNewOrder(self,order): 
         self.orders[order.order_id] = order
 
     def OnChangeOrder(self,change_order):
@@ -39,7 +39,7 @@ class OrderBook:
         print('\n')
 
         print('Sells:')
-        sellers.sort(key=lambda x:x.price,reverse=True)
+        sellers.sort(key=lambda x:x.price)
         self._print_orders(sellers)
 
 
@@ -52,4 +52,21 @@ class OrderBook:
                 agg_orders[single_order.price] = single_order.quantity
 
         for price,quantity in agg_orders.items():
-            print(f'Price: {price} | Quantity: {quantity}')
+            print(f'Price: {price}  Quantity: {quantity}')
+
+
+
+########### Example of the above code ###############
+            
+orderbook = OrderBook()
+
+# Addition of Order
+orderbook.OnNewOrder(EachOrder(1,'B',200,8))
+orderbook.OnNewOrder(EachOrder(2,'B',198,5))
+orderbook.OnNewOrder(EachOrder(3,'S',204,5))
+orderbook.OnNewOrder(EachOrder(4,'S',202,5))
+orderbook.OnNewOrder(EachOrder(5,'S',202,1))
+
+#print the order
+
+orderbook.PrintOrderBook()
