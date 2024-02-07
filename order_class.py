0dadc4689d63db455456684e07fa4c7b5420d69c
Ravi <ravikumarsinha234@gmail.com>
2024-02-07 21:04:50 +0530

    delete and print order

--- order_class.py
+++ order_class.py
@@ -17,3 +17,39 @@ class OrderBook:
             order = self.orders[change_order.order_id] #order to be updated
             order.price = change_order.new_price
             order.quantity = change_order.new_quantity
+
+    def OnCancelOrder(self,cancel_order):
+        if cancel_order.order_id in self.orders:
+            del self.orders[cancel_order.order_id]
+
+    def PrintOrderBook(self):
+        buyers = []
+        sellers = []
+
+        for order_id,order_details in self.orders.items():
+            if order_details.side=='B':
+                buyers.append(order_details)
+            else:
+                sellers.append(order_details)
+
+        print('Buys:')
+        buyers.sort(key=lambda x:x.price,reverse=True)
+        self._print_orders(buyers)
+
+        print('\n')
+
+        print('Sells:')
+        sellers.sort(key=lambda x:x.price,reverse=True)
+        self._print_orders(sellers)
+
+
+    def _print_orders(self,orders):
+        agg_orders = {}
+        for single_order in orders:
+            if single_order.price in agg_orders:
+                agg_orders[single_order.price] += single_order.quantity
+            else:
+                agg_orders[single_order.price] = single_order.quantity
+
+        for price,quantity in agg_orders.items():
+            print(f'Price: {price} | Quantity: {quantity}')
